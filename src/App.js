8698b8738cf62964f34d3dc073636403cc05509f
shoaib900 <rshoaib376@gmail.com>
2026-04-06 11:11:28 +0500

    update react js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import Child from "./components/Child"
-import img1 from "./assets/123.png"
+import React from 'react'
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom'
+import Home from './routes/Home'
+import About from './routes/About'
 
 const App = () => {
-  var name = "adil"
-  var age = 22
   return (
+    <BrowserRouter>
     <div>
-      <h1 style={{backgroundColor:"red"}}>hello world </h1>
-      <h2>my name is {name} and my age is {age} </h2>
-      <img src={img1} width={200} alt="" />
-      <div>
-        {4 + 4}
-      </div>
+      <ul>
+        <li><Link to={"/"}>Home</Link></li>
+        <li><Link to={"/about"}>About</Link></li>
+      </ul>
 
-      <Child />
+      <Routes>
+        <Route path="/" element={<Home /> } />
+        <Route path='/about' element={<About /> } />
+      </Routes>
       
     </div>
+    </BrowserRouter>
   )
 }
 
