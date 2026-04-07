81b16d6bde8debf8c38936a75075a8203674d003
shoaib900 <rshoaib376@gmail.com>
2026-04-07 10:44:42 +0500

    update api state effect hook

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import style from "./about.module.css"
 const About = () => {
+  const [data,setData] = useState([]);
+
+
+  React.useEffect(()=>{
+    fetch("https://api.escuelajs.co/api/v1/products")
+     .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.log(error))
+  },[]);
+
+  console.log(data)
+
   return (
     <div>
         <h1>About page</h1>
+        <div className={style.container}>
+        {
+          data.map( (item,index)=>{
+            return(
+              <div className={style.card}>
+                <h3>{item.title}</h3>
+                <p>{item.price}</p>
+                <img src={item.images[0]} height={200} alt="" />
+
+              </div>
+            )
+          })
+        }
+        </div>
       
     </div>
   )
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Home = () => {
+  const [count,setCount] = useState(0)
+  const [errmsg,setErrmsg] = useState("");
+
+  function dec(){
+    if(count === 0){
+      setCount(0);
+      console.log("that value can't be -ive")
+      setErrmsg("that value can't be -ive")
+    }else{
+      setCount(count - 1)
+    }
+  }
   return (
     <div>
-        <h1>Home page</h1>
+        <h1>Home page {count} </h1>
+        <span style={{color:"red"}}>{errmsg} </span><br />
+
+        <button onClick={()=> {setCount(count + 1); setErrmsg("")}}>add by 1</button>
+        <button onClick={dec}>Decrement by 1</button>
       
     </div>
   )
